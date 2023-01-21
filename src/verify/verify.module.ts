c52aee1435256316b67e00dcf3523297927cbda3
Sebastián Parra <jsparram17@gmail.com>
2023-01-21 15:55:57 -0500

    Finish verify implementation ToDo => Unit test

--- src/verify/verify.module.ts
+++ src/verify/verify.module.ts
@@ -1,20 +1,21 @@
-import {Module} from '@nestjs/common';
-import {VerifyController} from '../controllers/verify.controller';
-import {VerifyService} from '../services/verify.service';
+import { Module } from "@nestjs/common";
+import { VerifyController } from "./controllers/verify.controller";
+import { VerifyService } from "./services/verify.service";
+import configuration from "../../config/configuration";
 
 @Module({
-    controllers: [VerifyController],
-    providers: [
-        {
-            provide: 'TwilioClient',
-            useFactory: async () => {
-                return require('twilio')(
-                    'ACa4234bf9038ae0707ef0ad68b2fa6fe3',
-                    'dd420bd1cb83d6f588c8f49a2fbbafd0',
-                );
-            }
-        },
-        VerifyService]
+  controllers: [VerifyController],
+  providers: [
+    {
+      provide: "TwilioClient",
+      useFactory: async () => {
+        return require("twilio")(
+          configuration().twilio_account_sid,
+          configuration().twilio_account_token
+        );
+      }
+    },
+    VerifyService]
 })
 export class VerifyModule {
 }
