f6b69067e37dc5fc0a703f71c9fb637746163627
Sebastián Parra <jparr31@bancodebogota.com.co>
2023-10-01 21:22:01 -0500

    delete verify temporary

--- src/modules/verify/controllers/verify.controller.ts
+++ src/modules/verify/controllers/verify.controller.ts
@@ -12,14 +12,14 @@ export class VerifyController {
   @Post()
   public async sendVerifyNotification(@Headers() headers): Promise<void> {
     this.logger.log(`INICIA ENVIO DE OTP DE VERIFICACIÓN POR ${headers.channel.toUpperCase()} CON DESTINATARIO ${headers.recipient}`);
-    await this.verifyService.sendOtp(headers.channel, headers.recipient);
+    //await this.verifyService.sendOtp(headers.channel, headers.recipient);
     this.logger.log(`FINALIZA ENVIO DE OTP DE VERIFICACIÓN POR ${headers.channel.toUpperCase()} CON DESTINATARIO ${headers.recipient}`);
   }
 
   @Post("/verify")
   public async verifyOtp(@Headers() headers): Promise<void> {
     this.logger.log(`INICIA VALIDACIÓN DE OTP [CÓDIGO: ${headers.otp}]`);
-    await this.verifyService.validateOtp(headers.otp, headers.recipient);
+    //await this.verifyService.validateOtp(headers.otp, headers.recipient);
     this.logger.log(`FINALIZA VALIDACIÓN DE OTP [CÓDIGO: ${headers.otp}]`);
   }
 }
