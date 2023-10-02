f75afaaebd879f08578f40151ae5bf47e081a6b7
Sebastián Parra <jparr31@bancodebogota.com.co>
2023-10-01 21:17:50 -0500

    add log to test

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { CoreModule } from './modules/core.module';
 
 async function bootstrap() {
+  console.log("--- INIT ---")
   const app = await NestFactory.create(CoreModule);
   await app.listen(3000);
 }
