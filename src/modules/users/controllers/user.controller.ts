d882079bf5024fc0e8f05fc2f33a6b2bae64171b
Sebastián Parra <jparr31@bancodebogota.com.co>
2023-10-03 20:44:55 -0500

    return errors in user controller;

--- src/modules/users/controllers/user.controller.ts
+++ src/modules/users/controllers/user.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Get, Headers, Logger, Post, Put} from "@nestjs/common";
-import {UserService} from '../services/user.service';
+import { Body, Controller, Get, Headers, Logger, Post, Put } from "@nestjs/common";
+import { UserService } from '../services/user.service';
 import { User } from "src/database/entities/User";
+import { UserException } from "src/exceptions/user_exception";
 
 @Controller("user")
 export class UserController {
@@ -13,38 +14,59 @@ export class UserController {
     //OK - 27/08/2023
     @Post("client")
     public async createClientInDb(@Body() user: User): Promise<void> {
-        this.logger.log(`[CC: ${user.documentNumber}] INICIA CREACIÓN DE CLIENTE EN BASE DE DATOS`);
-        await this.userService.saveClient(user);
-        this.logger.log(`[CC: ${user.documentNumber}] FINALIZA CREACIÓN DE CLIENTE EN BASE DE DATOS`);
+        this.logger.log(`[CC: ${user.cellphone}] INICIA CREACIÓN DE CLIENTE EN BASE DE DATOS`);
+        await this.userService.saveClient(user)
+            .then(() => {
+                this.logger.log(`[CC: ${user.cellphone}] FINALIZA CREACIÓN DE CLIENTE EN BASE DE DATOS`);
+            })
+            .catch(err => {
+                throw new UserException(err.message, err.status);
+            });
     }
 
     //OK - 27/08/2023
     @Post("worker")
     public async createWorkerInDb(@Body() body): Promise<void> {
-        const user = body.user; 
+        const user = body.user;
         const category = body.category;
 
-        this.logger.log(`[CC: ${user.documentNumber}] INICIA CREACIÓN DE PROFESIONAL EN BASE DE DATOS`);
-        await this.userService.saveWorker(user, category);
-        this.logger.log(`[CC: ${user.documentNumber}] FINALIZA CREACIÓN DE PROFESIONAL EN BASE DE DATOS`);
+        this.logger.log(`[CC: ${user.cellphone}] INICIA CREACIÓN DE PROFESIONAL EN BASE DE DATOS`);
+        await this.userService.saveWorker(user, category)
+            .then(() => {
+                this.logger.log(`[CC: ${user.cellphone}] FINALIZA CREACIÓN DE PROFESIONAL EN BASE DE DATOS`);
+
+            })
+            .catch(err => {
+                throw new UserException(err.message, err.status);
+            });
     }
 
     //OK - 27/08/2023
     @Get()
     public async userInfo(@Headers() headers) {
         this.logger.log(`[CC: ${headers.document_number}] INICIA OBTENCIÓN DE LA INFORMACIÓN DEL USUARIO`);
-        const user = await this.userService.getUserInfo(headers.cellphone);
-        this.logger.log(`[CC: ${headers.document_number}] FINALIZA OBTENCIÓN DE LA INFORMACIÓN DEL USUARIO`);
-        return user;
+        return this.userService.getUserInfo(headers.cellphone)
+            .then((user) => {
+                this.logger.log(`[CC: ${headers.document_number}] FINALIZA OBTENCIÓN DE LA INFORMACIÓN DEL USUARIO`);
+                return user;
+            })
+            .catch(err => {
+                throw new UserException(err.message, err.status);
+            });
     }
 
     //OK - 27/08/2023
     @Get("exists")
     public async userExistsInDb(@Headers() headers): Promise<boolean> {
-        this.logger.log(`[CEL: ${headers.cellphone}] INICIA CONSULTA DE REGISTRO DEL USUARIO`);
-        const userExists = await this.userService.checkUserExists(headers.cellphone);
-        this.logger.log(`[CEL: ${headers.cellphone}] FINALIZA CONSULTA DE REGISTRO DEL USUARIO CON RESULTADO`);
-        return userExists;
+        this.logger.log(`[CEL: ${headers.cellphone}] INICIA CONSULTA DE EXISTENCIA DE USUARIO`);
+        return this.userService.checkUserExists(headers.cellphone)
+            .then((user) => {
+                this.logger.log(`[CEL: ${headers.cellphone}] FINALIZA CONSULTA DE EXISTENCIA DE USUARIO CON RESULTADO`);
+                return user;
+            })
+            .catch(err => {
+                throw new UserException(err.message, err.status);
+            });
     }
 
     //TODO PENDING
